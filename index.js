f9f0b984723386d1c3f8917c1c09d8e948311b8b
vittoboa <boarinivittorio@gmail.com>
2022-12-12 19:06:01 +0100

    Respond with all posts of the user with the given id, including up to 5 comments for each post

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.listen(
     () => console.log(`API is listening on port ${PORT}`)
 );
 
-// return all posts of the user with the given id
+// return all posts of the user with the given id, including up to 5 comments for each post
 app.get('/users/:userId/custom-posts', async (req, res) => {
     const { userId } = req.params;
 
     // fetch all posts of the user with the given id
     const posts_res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts/`);
-    const posts = await posts_res.json();
+    let posts = await posts_res.json();
 
     // respond with 404 code if there are no posts for the given user
     if (!posts.length) {
@@ -21,6 +21,16 @@ app.get('/users/:userId/custom-posts', async (req, res) => {
         return;
     }
 
-    // respond with the list of posts
+    // add comments to each post
+    for (let post of posts) {
+        // fetch all comments of the post
+        const comments_res = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments/`);
+        const comments = await comments_res.json();
+
+        // add at most the first 5 comments
+        post.comments = comments.slice(0, 5);
+    }
+
+    // respond with the list of custom posts
     res.json(posts);
 });
