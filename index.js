bf810389222f909e79c5180f93481ef1fbfde442
vittoboa <boarinivittorio@gmail.com>
2022-12-12 18:41:39 +0100

    Add GET endpoint to return all posts of a user with a given id

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ app.listen(
     PORT,
     () => console.log(`API is listening on port ${PORT}`)
 );
+
+// return all posts of the user with the given id
+app.get('/users/:userId/custom-posts', async (req, res) => {
+    const { userId } = req.params;
+
+    // fetch all posts of the user with the given id
+    const posts_res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts/`);
+    const posts = await posts_res.json();
+
+    // respond with the list of posts
+    res.json(posts);
+});
